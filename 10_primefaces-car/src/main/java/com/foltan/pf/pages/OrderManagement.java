fd02c770f9802a9461a5486833ef1b74d6315aab
AndrejF6512 <89728570+AndrejF6512@users.noreply.github.com>
2023-03-23 11:34:36 +0100

    Update OrderManagement.java

--- 10_primefaces-car/src/main/java/com/foltan/pf/pages/OrderManagement.java
+++ 10_primefaces-car/src/main/java/com/foltan/pf/pages/OrderManagement.java
@@ -17,10 +17,7 @@ import javax.inject.Named;
 import org.primefaces.PrimeFaces;
 import org.primefaces.context.RequestContext;
 
-/**
- *
- * @author SIGINT-X
- */
+
 @ViewScoped
 @Named
 public class OrderManagement implements Serializable {
